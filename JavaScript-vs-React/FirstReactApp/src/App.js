8c15261fb615af2454a095d03c52902a340d9315
renatojo <renatojose.mo@gmail.com>
2024-11-27 19:46:40 -0300

    atividades

--- JavaScript-vs-React/FirstReactApp/src/App.js
+++ JavaScript-vs-React/FirstReactApp/src/App.js
@@ -22,6 +22,13 @@ const content = [
   [
     "Projetos 'Vanilla JavaScript' requer uma programação imperativa, isto é, necessita-se da descriação de cada detalhe, passo-a-passo para alcançar um resultado",
     "Já o React, abrange a programação declarativa, isto que dizer que: com o React, você define seu objetivo e o React definie (se resolve) para alcançá-lo!"
+  ],
+  [
+    "Facebook",
+    "Instagram",
+    "Netflix",
+    "Airbnb",
+    "Walmart",
   ]
 ];
 
@@ -57,6 +64,18 @@ export default function App() {
             onClick={() => setActiveContentIndex(2)}
           >
             Assunto Relacionado e Referência
+          </button>
+          <button
+            className={activeContentIndex === 3 ? "active" : ""}
+            onClick={() => setActiveContentIndex(3)}
+          >
+            Diferenças entre React e Vanilla JS
+          </button>
+          <button 
+            className={activeContentIndex === 4 ? "active" : ""}
+            onClick={() => setActiveContentIndex(4)}
+            >
+              Empresas que usam React
           </button>
         </menu>
         <div id="tab-content">
