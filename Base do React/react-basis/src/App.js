c07dabd97cf4bdd6c570c6839365cec0f20f917a
renatojo <renatojose.mo@gmail.com>
2024-11-27 22:05:31 -0300

    estrutura do react

---
+++ Base do React/react-basis/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import NovoComponente from './novo-componente/novo-componente';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <NovoComponente/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Base do React/react-basis/src/novo-componente/novo-componente.js
@@ -0,0 +1,12 @@
+import './novo-componente.css';
+
+const NovoComponente = () => {
+
+    return (
+        <div>
+            <p>texto em novo componente</p>
+        </div>
+    )
+}
+
+export default NovoComponente;
