3cedda27bd48640f585474a3770f4c495299dce9
renatojo <renatojose.mo@gmail.com>
2024-11-29 22:03:25 -0300

    resolução de atividade com react

--- Base do React/react-basis/src/components/header/Header.jsx
+++ Base do React/react-basis/src/components/header/Header.jsx
@@ -1,13 +1,19 @@
 import './Header.css';
 
+const RandomWords = () => {
+    const words = ["conceitos", "fundamentos", "definição"]
+    const indice = Math.floor(Math.random() * words.length)
+    return words[indicie]
+}
+ 
 const Header = () => {
     return(
+        <>
       <header>
           <h1>Fundamentos do React</h1>
-          <p>
-            Iremos aplicar aqui conceitos praticos do react
-          </p>
+          <p>Iremos aplicar aqui {RandomWords()} praticos do react</p>
         </header>
+        </>
     )
   }
 
--- Base do React/react-basis/src/components/main/components/MeuObjeticoComReact.jsx
+++ Base do React/react-basis/src/components/main/components/MeuObjeticoComReact.jsx
@@ -1,13 +1,18 @@
+let meusobjetivos = ["Criar interfaces dinâmicas", "Componentizar e reutilizar código", "Criar aplicações de página única (SPAs)."]
+let txt = "";
+
+function Function(value) {
+    
+}
+
 const MeuObjetivoComReact = () => {
     return(
+        <>
         <div>
             <h3 class="lista">Meus objetivos com react são:</h3>
-            <ul>
-                <li class="lista">Criar interfaces dinâmicas</li>
-                <li class="lista">Componentizar e reutilizar código</li>
-                <li class="lista">Criar aplicações de página única (SPAs).</li>
-            </ul>
+            <Function />
         </div>
+        </>
     )
 }
 
